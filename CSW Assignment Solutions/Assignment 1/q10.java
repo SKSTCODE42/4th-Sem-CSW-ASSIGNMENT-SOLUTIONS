17d776c336fca06d541bc1ec576708a74f8c6f8c
SHAIKH TABREZ <100278516+SKSTCODE42@users.noreply.github.com>
2023-06-25 13:49:00 +0530

    Update q10.java

--- CSW Assignment Solutions/Assignment 1/q10.java
+++ CSW Assignment Solutions/Assignment 1/q10.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 public class q10 {
     public static void maxMin(int[] arr) {
         int n = arr.length;
@@ -18,7 +17,6 @@ public class q10 {
             arr[i] = result[i];
         }
     }
-
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6 };
         System.out.println("Original array: " + Arrays.toString(arr));
