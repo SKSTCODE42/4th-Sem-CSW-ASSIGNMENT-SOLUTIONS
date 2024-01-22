457d0fa3076f22eeb78bae4e8b0e7aebc36af84d
SHAIKH TABREZ <100278516+SKSTCODE42@users.noreply.github.com>
2024-01-22 08:55:43 +0530

    Update q5.java

--- CSW Assignment Solutions/Assignment 2B/q5.java
+++ CSW Assignment Solutions/Assignment 2B/q5.java
@@ -2,7 +2,6 @@ import java.util.LinkedHashSet;
 
 public class q5 {
     public static void main(String[] args) {
-     
         LinkedHashSet<Double> set = new LinkedHashSet<Double>();
         // Insert some elements into the linked hash set
         set.add(2.5);
