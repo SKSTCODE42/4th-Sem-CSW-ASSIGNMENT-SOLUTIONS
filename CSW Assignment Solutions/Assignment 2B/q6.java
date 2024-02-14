721b71ef0a4efd53d95915a5552bf540afbf8e8b
SHAIKH TABREZ <100278516+SKSTCODE42@users.noreply.github.com>
2024-02-14 08:24:45 +0530

    Update q6.java

--- CSW Assignment Solutions/Assignment 2B/q6.java
+++ CSW Assignment Solutions/Assignment 2B/q6.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class q6 {
     public static void main(String[] args) {
-        // Create a hash map
+
         HashMap<Integer, String> map = new HashMap<Integer, String>();
         // Insert some elements into the hash map
         map.put(1, "apple");
